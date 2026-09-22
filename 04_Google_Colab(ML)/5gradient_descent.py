bb2a4f87544731edc680d14fb4a1211735a94de2
Abhay Bhardwaj <abhaybhardwaj15704@gmail.com>
2026-09-22 17:53:46 +0530

    about joblid and Pickle

--- 04_Google_Colab(ML)/5gradient_descent.py
+++ 04_Google_Colab(ML)/5gradient_descent.py
@@ -8,11 +8,12 @@ def gradient_descent(x, y):
 
     for i in range(iteration):
         y_predicted = m_curr * x + b_curr
+        cost = (1/n) * sum([val**2 for val in (y - y_predicted)])
         md = -(2/n)*sum(x*(y-y_predicted))
         bd = -(2/n)*sum(y-y_predicted)
         m_curr = m_curr - learning_rate * md
         b_curr = b_curr - learning_rate * bd
-        print("m {}, b {}, itergatin {}". format(m_curr, b_curr, iteration))
+        print("m {}, b {},cost {}, itergatin {}". format(m_curr, b_curr, cost, iteration))
 
 x = np.array([1,2,3,4,5])
 y = np.array([5,7,9,11,13])
